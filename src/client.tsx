ad331bea18c2c35cdadfd171066757d85142a8b9
Maxwell Twente <maxtwente@gmail.com>
2023-05-23 09:44:31 -0500

    added function to get docs

--- src/client.tsx
+++ src/client.tsx
@@ -17,7 +17,59 @@ export class FastAPIClient {
       throw error;
     }
   }
+  public async getDocumentsPaged(
+    collections: string,
+    pageSize: number,
+    pageNumber: number,
+    startTime: string | null = "",
+    endTime: string | null = "",
+    longStart: number | null = 0,
+    longEnd: number | null = 0,
+    latStart: number | null = 0,
+    latEnd: number | null = 0,
+    country: string | null = "",
+    type: string | null = "",
+    attribute1Start: number | null = 0,
+    attribute1End: number | null = 0,
+    attribute2Start: number | null = 0,
+    attribute2End: number | null = 0,
+    includeEdges: boolean | null = false,
+    edgeCollection: string | null = "",
+    excludeEdges: boolean | null = false,
+    collectionFilter: string | null = ""
+  ): Promise<any> {
+    try {
+      const response: AxiosResponse<any> = await axios.get(`${this.baseURL}/get_documents_paged`, {
+        params: {
+          collections,
+          pageSize,
+          pageNumber,
+          startTime,
+          endTime,
+          longStart,
+          longEnd,
+          latStart,
+          latEnd,
+          country,
+          type,
+          attribute1Start,
+          attribute1End,
+          attribute2Start,
+          attribute2End,
+          includeEdges,
+          edgeCollection,
+          excludeEdges,
+          collectionFilter
+        }
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching paged documents:', error);
+      throw error;
+    }
+  }
 }
+
 
 // // usage example
 // const client = new FastAPIClient('https://example.com/api');
